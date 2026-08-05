ab86f6d2c8ef85bf0172d755962da1616e3cdef2
Adraj-tech <adarshraj22336@gmail.com>
2026-08-06 02:10:28 +0530

    Time: 0 ms (100%), Space: 40.3 MB (61.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -9,14 +9,14 @@ public:
 
         for (int i = 0; i < n; i++) {
             answer[i] = prefix;
-            prefix *= nums[i];
+            prefix =prefix * nums[i];
         }
 
         int suffix = 1;
 
         for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;
-            suffix *= nums[i];
+            answer[i] =answer[i] * suffix;
+            suffix =suffix * nums[i];
         }
 
         return answer;
