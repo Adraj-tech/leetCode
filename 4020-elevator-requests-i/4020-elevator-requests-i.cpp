f56249538b7e536411c41d32d2ac45cbde34866b
Adraj-tech <adarshraj22336@gmail.com>
2026-08-16 08:54:09 +0530

    Time: 0 ms (100%), Space: 31.1 MB (33.33%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.cpp
+++ 4020-elevator-requests-i/4020-elevator-requests-i.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int elevatorRequests(int n, vector<int>& requests) {
-        int m = requests.size();
+    int elevatorRequests(int n, vector<int>& requests) {int time = 0;
         int atFloor = 0;
-        int time = 0;
-        for (int i = 0; i < m; i++) {
-            time = time + abs(atFloor - requests[i]);
-            atFloor = requests[i];
+        for (int floor : requests) {
+            time += abs(atFloor - floor);
+            atFloor = floor;
         }
         return time;
+
+        
+        // int m = requests.size();
+        // int atFloor = 0;
+        // int time = 0;
+        // for (int i = 0; i < m; i++) {
+        //     time = time + abs(atFloor - requests[i]);
+        //     atFloor = requests[i];
+        // }
+        // return time;
     }
 };
