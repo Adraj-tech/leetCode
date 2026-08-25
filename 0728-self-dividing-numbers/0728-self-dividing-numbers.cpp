a1ff84ec5737896c090ccbf52654d26ea5c13438
Adraj-tech <adarshraj22336@gmail.com>
2026-08-25 20:03:37 +0530

    Time: 0 ms (100%), Space: 9.3 MB (26.94%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        for(int i = left; i<=right; i++){
+            int res = 1;
+            int n = i;
+            while(n>0){
+                int digit = n%10;
+                if(digit == 0 || i%digit != 0){
+                    res = 0;
+                    break;
+                } 
+                n = n/10;
+            }
+            if(res == 1)
+            result.push_back(i);
+        }
+        return result;
+    }
+};
