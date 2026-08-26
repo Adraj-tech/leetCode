aed670f991d9b3571508a12f3db5431d1dec954e
Adraj-tech <adarshraj22336@gmail.com>
2026-08-26 15:57:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         if(s.size() != t.size()){
             return 0;
         }
-        vector<int> freq(26);
+        int freq[26] = {0};
         for(int i = 0; i<s.size(); i++){
             freq[s[i] - 'a']++;
             freq[t[i] - 'a']--;
