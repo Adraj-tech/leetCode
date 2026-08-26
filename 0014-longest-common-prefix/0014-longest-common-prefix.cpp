40c4bd2700442a24579c1c4526dbd226063b2b68
Adraj-tech <adarshraj22336@gmail.com>
2026-08-26 15:41:55 +0530

    Time: 0 ms (100%), Space: 11.9 MB (58.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,8 +4,7 @@ public:
         string ans = strs[0];
         for (int i = 1; i < strs.size(); i++) {
             int j = 0;
-            while (j < ans.size() && j < strs[i].size() &&
-                   ans[j] == strs[i][j]) {
+            while (j < ans.size() && j < strs[i].size() && ans[j] == strs[i][j]) {
                 j++;
             }
             ans = ans.substr(0, j);
