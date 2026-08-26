993456f56789713b2479a3d11336c520ab90e028
Adraj-tech <adarshraj22336@gmail.com>
2026-08-26 15:58:23 +0530

    Time: 1 ms (54.11%), Space: 9.6 MB (75.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return 0;
-        }
+        if(s.size() != t.size()) return 0;
         int freq[26] = {0};
         for(int i = 0; i<s.size(); i++){
             freq[s[i] - 'a']++;
