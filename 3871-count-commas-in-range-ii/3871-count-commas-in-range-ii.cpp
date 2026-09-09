411db5663547e4de2b584e26825439306aa9dfba
Adraj-tech <adarshraj22336@gmail.com>
2026-09-09 16:34:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans = 0;
-        long long threshold = 1000;
-
-        while (threshold <= n) {
-            ans += n - threshold + 1;
-            threshold *= 1000;
+        for (long long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
         }
-
         return ans;
     }
 };
