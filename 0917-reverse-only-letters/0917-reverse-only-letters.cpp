87e7d5d9c75610ce7c4b77f939a76c12a76408ed
Adraj-tech <adarshraj22336@gmail.com>
2026-09-10 20:32:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.29%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string reverseOnlyLetters(string s) {
+
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (!((s[i] >= 'A' && s[i] <= 'Z') ||
+                  (s[i] >= 'a' && s[i] <= 'z'))) {
+                i++;
+            }
+            else if (!((s[j] >= 'A' && s[j] <= 'Z') ||
+                       (s[j] >= 'a' && s[j] <= 'z'))) {
+                j--;
+            }
+            else {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+
+        return s;
+    }
+};
