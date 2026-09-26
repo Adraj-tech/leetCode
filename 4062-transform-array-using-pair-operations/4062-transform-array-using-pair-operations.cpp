9cf030414ad345a57573ecdb1466ec8a294fe576
Adraj-tech <adarshraj22336@gmail.com>
2026-09-27 00:15:12 +0530

    Time: 7 ms (10.22%), Space: 193.1 MB (96.22%) - LeetHub

---
+++ 4062-transform-array-using-pair-operations/4062-transform-array-using-pair-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canTransform(vector<int>& source, vector<int>& target) {
+        long long sum1 = 0;
+        long long sum2 = 0;
+        for (int i = 0; i < source.size(); i++) {
+            sum1 = sum1 + source[i];
+        }
+        for (int i = 0; i < target.size(); i++) {
+            sum2 = sum2 + target[i];
+        }
+        if (sum1 == sum2)
+            return 1;
+        return 0;
+    }
+};
